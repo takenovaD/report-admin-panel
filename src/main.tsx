8fcd87c8e608d3543370823bcd47924565f9c8bc
takenovaD <125312288+takenovaD@users.noreply.github.com>
2025-09-07 21:24:18 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";  // добавляем
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/report-admin-panel">   {/* 👈 говорим роутеру, что сайт в подпапке */}
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
